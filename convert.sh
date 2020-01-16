dd60696de306d908070db3aec881d8ca021b011b
Marton Bur <marton.bur@mail.mcgill.ca>
2020-01-16 16:16:45 -0500

    Adding missing main file, PDF conversion turned off

--- convert.sh
+++ convert.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 mkdir -p output
-
-asciidoctor -b html5 ECSE-321-Tutorials.adoc -o output/index.html
-asciidoctor-pdf ECSE-321-Tutorials.adoc
-mv ECSE-321-Tutorials.adoc output/
-
+asciidoctor -b html5 ECSE-321-Tutorials.adoc -o index.html
+mv index.html output/
 cp -r figs output/
